2c6b9d63c7c60bcd888dfa81ef94ee66c2973295
Alec Cunningham <aleccunningham96@gmail.com>
2021-12-12 15:49:05 -0600

    chore(docs) update database design

--- docs/DESIGN.md
+++ docs/DESIGN.md
@@ -5,6 +5,12 @@
 Store the JSON within the binary due to the size and immutability of the dataset.
 The web library, Gin, handles each HTTP request as a separate Goroutine, making the retrieval of the key upon each request extremely fast and allows for up to millions of rps.
 
+- Storing the data as a model in a traditional relational database introduces unrequired complexity and additional latency
+
+- Storing the data in cache using an in-memory cache library persists the data per
+pod restarts, so additional work to add an `initContainer` for seeding data would
+be required. It also introduces an external dependency to read the data.
+
 ## Scalability
 
 Using the default values in the Helm chart, the API is deployed with three replicas,
--- main.go
+++ main.go
@@ -2,9 +2,65 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-// StateCode represents data about a record statecode.
+// stateCodes slice to seed US State code data.
+var stateCodes = []StateCode{
+	{State: "Alabama", Abbreviation: "AL"},
+	{State: "Alaska", Abbreviation: "AK"},
+	{State: "Arizona", Abbreviation: "AZ"},
+	{State: "Arkansas", Abbreviation: "AR"},
+	{State: "California", Abbreviation: "CA"},
+	{State: "Colorado", Abbreviation: "CO"},
+	{State: "Connecticut", Abbreviation: "CT"},
+	{State: "Delaware", Abbreviation: "DE"},
+	{State: "District of Columbia", Abbreviation: "DC"},
+	{State: "Florida", Abbreviation: "FL"},
+	{State: "Georgia", Abbreviation: "GA"},
+	{State: "Hawaii", Abbreviation: "HI"},
+	{State: "Idaho", Abbreviation: "ID"},
+	{State: "Illinois", Abbreviation: "IL"},
+	{State: "Indiana", Abbreviation: "IN"},
+	{State: "Iowa", Abbreviation: "IA"},
+	{State: "Kansas", Abbreviation: "KS"},
+	{State: "Kentucky", Abbreviation: "KY"},
+	{State: "Lousiana", Abbreviation: "LA"},
+	{State: "Maine", Abbreviation: "ME"},
+	{State: "Montana", Abbreviation: "MT"},
+	{State: "Nebraska", Abbreviation: "NE"},
+	{State: "Nevada", Abbreviation: "NV"},
+	{State: "New Hampshire", Abbreviation: "NH"},
+	{State: "New Jersey", Abbreviation: "NJ"},
+	{State: "New Mexico", Abbreviation: "NM"},
+	{State: "New York", Abbreviation: "NY"},
+	{State: "North Carolina", Abbreviation: "NC"},
+	{State: "North Dakota", Abbreviation: "ND"},
+	{State: "Ohio", Abbreviation: "OH"},
+	{State: "Oklahoma", Abbreviation: "OK"},
+	{State: "Oregon", Abbreviation: "OR"},
+	{State: "Maryland", Abbreviation: "MD"},
+	{State: "Massachusetts", Abbreviation: "MA"},
+	{State: "Michigan", Abbreviation: "MI"},
+	{State: "Minnesota", Abbreviation: "MN"},
+	{State: "Missouri", Abbreviation: "MO"},
+	{State: "Pennsylvania", Abbreviation: "PA"},
+	{State: "Rhode Island", Abbreviation: "RI"},
+	{State: "South Carolina", Abbreviation: "SC"},
+	{State: "South Dakota", Abbreviation: "SD"},
+	{State: "Tennessee", Abbreviation: "TN"},
+	{State: "Texas", Abbreviation: "TX"},
+	{State: "Utah", Abbreviation: "UT"},
+	{State: "Vermont", Abbreviation: "VT"},
+	{State: "Virginia", Abbreviation: "VA"},
+	{State: "Washington", Abbreviation: "WA"},
+	{State: "West Virginia", Abbreviation: "WV"},
+	{State: "Wisconsin", Abbreviation: "WI"},
+	{State: "Wyoming", Abbreviation: "WY"},
+}
+
+// StateCode represents data about a statecode.
 type StateCode struct {
 	State        string `json:"state"`
 	Abbreviation string `json:"abbreviation"`
@@ -14,8 +70,8 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/statecode", getStateCodes)
-	router.GET("/statecode/:id", getStateCodeByID)
-	router.Run("localhost:8080")
+	router.GET("/statecode/:state", getStateCodeByID)
+	router.Run(":8080")
 }
 
 // getStateCodes responds with the list of all statecodes as JSON.
@@ -23,7 +79,7 @@ func getStateCodes(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, stateCodes)
 }
 
-// getAlbumByID locates the state code whose ID value matches the id
+// getStateCodeByID locates the state code whose ID value matches the id
 // parameter sent by the client, then returns that statecode as a response.
 func getStateCodeByID(c *gin.Context) {
 	state := c.Param("state")
@@ -32,7 +88,7 @@ func getStateCodeByID(c *gin.Context) {
 	// a state code whose ID value matches the parameter.
 	for _, a := range stateCodes {
 		if a.State == state {
-			c.IndentedJSON(http.StatusOK, a)
+			c.IndentedJSON(http.StatusOK, a.Abbreviation)
 			return
 		}
 	}
