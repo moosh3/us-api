78fe46e41513a5081531ad4520266df117377a18
Alec Cunningham <aleccunningham96@gmail.com>
2021-12-12 18:30:29 -0600

    fix: add chart back to git

--- infrastructure/terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  default     = "us-east-1"
-  description = "Default AWS Region"
-}
-
-variable "vpc_cidr" {
-  default     = "172.20.0.0/18"
-  description = "VPC CIDR range"
-}
